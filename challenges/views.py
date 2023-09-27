c42a3939de7ef9e6bf0b3396674d4233e3396ac6
Hamid <hamidarianyegane@gmail.com>
2023-09-28 00:21:07 +0330

    url patterns updated

--- challenges/views.py
+++ challenges/views.py
@@ -3,8 +3,15 @@ from django.http import HttpResponse
 
 
 # Create your views here.
-def january(request):
-    return HttpResponse("eat no meats for the entire of the month!")
 
-def february(request):
-    return HttpResponse("walk for at least 30 minutes every day!")
+def monthly_challenges(request, month):
+    monthly_challenge = None
+    if month == "february":
+        monthly_challenge = "walk for at least 30 minutes every day!"
+    elif month == "january":
+        monthly_challenge = "eat no meats for the entire of the month!"
+    elif month == "march":
+        monthly_challenge = "it's you're birthday month! just have fun :)"
+    else:
+        monthly_challenge = "this month is not defined"
+    return HttpResponse(monthly_challenge)
