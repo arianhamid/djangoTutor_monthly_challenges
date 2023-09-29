99af29757cca13df525e9b532a42e5e63707ec37
Hamid <hamidarianyegane@gmail.com>
2023-09-29 16:50:47 +0330

    url tag for dynamic urls added

--- challenges/views.py
+++ challenges/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
+from django.template.loader import render_to_string
 
 monthly_challenges = {
     "january": "walk for at least 30 minutes every day!",
@@ -21,12 +22,8 @@ monthly_challenges = {
 
 def index_challenge(request):
     months = list(monthly_challenges.keys())
-    list_items = ""
-    for month in months:
-        list_items += f"<li><a href=\"{reverse('challenges_url_name', args=[month])}\">{month}</a></li>"
-    return HttpResponse(f"<ul>{list_items}</ul>")
-
-
+    return render(request, 'challenges/index.html', {'months': months})
+    
 def monthly_challenges_by_number(request, month):
     months = list(monthly_challenges.keys())
     if month > len(months) or month < 1:
@@ -35,10 +32,9 @@ def monthly_challenges_by_number(request, month):
     redirect_path = reverse("challenges_url_name", args=[redirect_month])
     return HttpResponseRedirect(redirect_path)
 
-
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
-        return HttpResponse(challenge_text)
+        return render(request, 'challenges/challenge.html', {'challenge_text': challenge_text, 'title': month})
     except:
         return HttpResponseNotFound("this month is not supported")
