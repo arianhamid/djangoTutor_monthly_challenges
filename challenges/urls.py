b41c31394c9bea5af0d702793fe624e0c39e413a
Hamid <hamidarianyegane@gmail.com>
2023-09-29 21:38:29 +0330

    include tag added to templates

--- challenges/urls.py
+++ challenges/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.index_challenge),
+    path("", views.index_challenge, name="index"),
     path('<int:month>', views.monthly_challenges_by_number),
-    path('<str:month>', views.monthly_challenge, name = 'challenges_url_name'),
+    path('<str:month>', views.monthly_challenge, name='challenges_url_name'),
 ]
